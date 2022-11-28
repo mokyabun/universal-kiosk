ed3962e9fbcf5a5a2620da9e7ccb35c78396ee10
seonghwan7694 <seonghwan7694@gmail.com>
2022-11-28 13:46:01 +0900

    🐍 Add Django_project with initial settings

---
+++ Django_project/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf app/migrations/*
+rm -rf db.sqlite3
+rm -rf static/*
+
+touch app/migrations/__init__.py
+
+python manage.py makemigrations
+python manage.py migrate
+
+echo yes | python manage.py collectstatic
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'qwert1234')" | python manage.py shell
+python manage.py runserver
