119209bec1424f70679cd01ae176aa69a4bb3188
seonghwan7694 <seonghwan7694@gmail.com>
2022-12-13 21:09:21 +0900

    😁 Exclude "pins_arduino.h"

--- blind-device/esp8266/src/main.cpp
+++ blind-device/esp8266/src/main.cpp
@@ -3,12 +3,12 @@
 #include <WiFiClient.h>
 #include <Keypad.h>
 
-#include "pins_arduino.h"
 
 /*
 Constant variables
 */
 // Connection
+
 const char* ssid = "<WIFI SSID>";
 const char* password = "<WIFI PASSWORD>";
 
@@ -20,8 +20,8 @@ const int http_delay = 10000;  // 10 second delay
 // Keypad
 const byte rows = 4;
 const byte cols = 4;
-byte row_pins[rows] = {D0, D1, D2, D3};
-byte col_pins[cols] = {D4, D5, D6, D7};
+byte row_pins[rows] = {16, 5, 4, 0};
+byte col_pins[cols] = {2, 14, 12, 13};
 char keys[rows][cols] = {
     {'1','2','3','A'},
     {'4','5','6','B'},
@@ -35,26 +35,20 @@ char keys[rows][cols] = {
  * @param orders
  * @return int 
  */
-int send_order(int orders[], int order_count) {
-    if (!(orders[0] > 0)) return -1;
+int send_order(int order) {
     if (WiFi.status() != WL_CONNECTED) return -1;
 
     WiFiClient client;
     HTTPClient http;
     String send_message = "{\"orders\":";
 
-    for (int i = 0; i < order_count; i++) {
-        send_message += "[";
-        send_message += (char) order_count+'0';
-        send_message += ",";
-    }
-    send_message += "]}";
+    String server_path = server_url+String(order);
 
-    http.begin(client, server_url);
-
-    http.addHeader("Content-Type", "application/json");
-    int httpResponseCode = http.POST("{\"test\":\"test message\"}");
-
+    http.begin(client, server_path);
+  
+    int httpResponseCode = http.GET();
+    Serial.print("Sent to :");
+    Serial.println(server_path);
     Serial.print("HTTP Response code: ");
     Serial.println(httpResponseCode);
 
@@ -69,7 +63,7 @@ int send_order(int orders[], int order_count) {
  */
 void setup() {
     Serial.begin(9600);
-    
+    Serial.println("hello world!");
     // Connect WIFI
     WiFi.begin(ssid, password);
     Serial.print("Connecting to ");
@@ -91,8 +85,6 @@ void setup() {
 void loop() {
     static unsigned long delay_count = 0;
     static Keypad keypad = Keypad(makeKeymap(keys), row_pins, col_pins, rows, cols);
-    static int orders[100];
-    static int order_count = 0;
     static int order_number = 0;
 
     if (keypad.getKeys()) {
@@ -107,22 +99,12 @@ void loop() {
                     }
                     
                     else if (key_value == 'B') {
-                        orders[order_count] = order_number;
-                        order_count++;
+                        send_order(order_number);
+                        order_number = 0;
                     }
 
                     else if (key_value == 'C') {
-                        order_count--;
-                    }
-                    
-                    // http POST to server
-                    else if (key_value == 'D') {
-                        if (delay_count - millis() > http_delay) {
-                            if (send_order(orders, order_count) == -1) Serial.println("send error");
-                            else Serial.println("sended");
-
-                            delay_count = millis();
-                        }
+                        send_order(-1);
                     }
 
                     else if (key_value == '*') ;
