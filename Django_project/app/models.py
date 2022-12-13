8b6a6e9e6b4b413d44755b62b2ccc8a489c4f59e
seonghwan7694 <seonghwan7694@gmail.com>
2022-12-13 18:45:29 +0900

    ❌ Remove Django_project

--- Django_project/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Stuff(models.Model):
-    img = models.ImageField()
-    name = models.CharField(max_length=20)
-    price = models.IntegerField()
--- Django_project/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.main, name="main"),
-    path('order', views.order, name="order"),
-]
